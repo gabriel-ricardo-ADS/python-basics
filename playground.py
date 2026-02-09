ebeb6774619deb2b121fba8cb8025f989d7028d9
Gabriel Ricardo Gomes Lima <gabrielricardoglima@gmail.com>
2026-02-09 18:37:05 -0300

    Add files via upload

---
+++ playground.py
@@ -0,0 +1,19 @@
+# day 0 
+
+# testing basics
+   
+name = "Gabriel Ricardo Gomes Lima"
+age = 21
+
+#print(name, age)
+
+sent1 = "Hello, my name is"
+sent2 = "i am"
+sent3 = "years old"
+
+#print(sent1 + " " + name + " " + sent2 + str(age) + " " + sent3)
+
+#or
+
+print(f"{sent1} {name}, {sent2} {age} {sent3}.")
+
