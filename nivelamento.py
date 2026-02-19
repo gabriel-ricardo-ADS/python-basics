d609644d5c655a6fb37c5344f6437b8c1e7ddb49
Gabriel Ricardo Gomes Lima <gabrielricardoglima@gmail.com>
2026-02-19 18:17:57 -0300

    Add files via upload

--- nivelamento.py
+++ nivelamento.py
@@ -24,17 +24,42 @@
 #print("valor da venda: R$", venda)
 
 
-salario = float(input("digite o valor do salario: R$"))
-tempo = int(input("digite o tempo de serviço: "))
+#salario = float(input("digite o valor do salario: R$"))
+#tempo = int(input("digite o tempo de serviço: "))
 
-if tempo < 3:
-    salario = salario * 1.05
-elif tempo <= 5:
-    salario = salario * 1.10
-elif tempo <= 10:
-    salario = salario * 1.15
+#if tempo < 3:
+    #salario = salario * 1.05
+#elif tempo <= 5:
+    #salario = salario * 1.10
+#elif tempo <= 10:
+    #salario = salario * 1.15
 
-print("salario atualizado: R$", salario)
+#print("salario atualizado: R$", salario)
+
+
+#exemplo de imposto de renda:
+
+salario = float(input("valor do salario: R$"))
+if salario <= 1903.98:
+    ir = 0
+elif salario <= 2826.65:
+    ir = salario * 0.075 - 142.80
+elif salario <= 3751.05:
+    ir = salario * 0.15 - 354.80
+elif salario <= 4664.68:
+    ir = salario * 0.225 - 636.13
+else:
+    ir = salario * 0.275 - 869.36
+
+salario_liquido = salario - ir
+
+print("salario liquido: R$", salario_liquido)
 
         
+     
+
+
+
+
+
 
