3c12a8aebfaabde3a794c31e04fdf9a9812bcc4d
Gabriel Ricardo Gomes Lima <gabrielricardoglima@gmail.com>
2026-02-19 17:02:02 -0300

    Add files via upload

--- nivelamento.py
+++ nivelamento.py
@@ -13,27 +13,14 @@
 #print("desconto: R$" + str(desc))
 
 
-#venda = float(input("digite o valor da venda: R$"))
-
-#if venda > 300:
-    #desconto = venda * 0.10
-    #venda = venda - desconto
-
-#print("valor da venda: R$", venda)
 
 
-salario = float(input("digite o salario do funcionario: R$"))
-tempo = float(input("digite o tempo gasto em anos: "))
-if tempo <= 3:
-    salario = salario * 1.05
-elif tempo > 3 and tempo <= 5:
-    salario = salario * 1.10
-elif tempo > 5 and tempo <= 10:
-    salario = salario * 1.15
+venda = float(input("digite o valor da venda: R$"))
 
-    
-print("salario atualizado: R$", salario)
+if venda > 300:
+    desconto = venda * 0.10
+    venda = venda - desconto
 
-    
-    
+print("valor da venda: R$", venda)
+
 
