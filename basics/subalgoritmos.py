c046780bdae5cb7ebecafb6b6a0d8b379c3c8fde
Gabriel Ricardo Gomes Lima <gabrielricardoglima@gmail.com>
2026-02-20 10:04:47 -0300

    Add files via upload

---
+++ basics/subalgoritmos.py
@@ -0,0 +1,15 @@
+def saudacao():
+    print("Bem vindo à empresa!")
+
+def pessoal():
+    nome = input("Digite seu nome: ")
+    idade = int(input("Digite sua idade: "))
+    print(f"Olá, {nome}! Você tem {idade} anos.")
+
+
+saudacao()
+pessoal()
+
+    
+    
+        
