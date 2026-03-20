d837128e7f891a8076f81e93f37c37fd44c4121c
Gabriel Ricardo Gomes Lima <gabrielricardoglima@gmail.com>
2026-03-20 11:36:06 -0300

    Add files via upload

--- basics/Media_Aluno.py
+++ basics/Media_Aluno.py
@@ -1,37 +1,73 @@
-#Identificação do aluno
-nome = input("Digite seu nome: ")
-rm = input("Digite seu Rm: ")
-email = input("Digite seu email: ")
+while True:
 
-# input das notas das matérias 
-notaPython = float(input('Nota de Python: ').replace(",","."))
-notaJava = float(input('Nota de Java: ').replace(",","."))
-notaFront = float(input('Nota de Front End: ').replace(",","."))
-notaEng = float(input('Nota de Engenharia de Software: ').replace(",","."))
-notaData = float(input('Nota de Database: ').replace(",","."))
-notaIA = float(input('Nota de IA: ').replace(",","."))
+    #Identificação do aluno
+    print("----------IDENTIDADE---------")
+    nome = input("Digite seu nome: ")
+    rm = input("Digite seu Rm: ")
+    email = input("Digite seu email: ")
 
-notas_Aluno = {    # Dicionario de nome das matérias e notas
-    'Python': notaPython, 
-    'Java': notaJava, 
-    'Front End': notaFront, 
-    'Eng Soft': notaEng, 
-    'Database': notaData,
-    'IA': notaIA,
-}
+    # input das notas das matérias 
+    print("----------NOTAS----------")
+    notaPython = float(input('Nota de Python: ').replace(",","."))
+    notaJava = float(input('Nota de Java: ').replace(",","."))
+    notaFront = float(input('Nota de Front End: ').replace(",","."))
+    notaEng = float(input('Nota de Engenharia de Software: ').replace(",","."))
+    notaData = float(input('Nota de Database: ').replace(",","."))
+    notaIA = float(input('Nota de IA: ').replace(",","."))
+
+    print("---------FALTAS---------")
+    faltas = float(input("Digite quantas faltas você possui no semestre: "))
+
+    notas_Aluno = {
+        'Python': notaPython, 
+        'Java': notaJava, 
+        'Front End': notaFront, 
+        'Eng Soft': notaEng, 
+        'Database': notaData,
+        'IA': notaIA,
+    }
+        
+    # calculo da média
+    media_notas = sum(notas_Aluno.values()) / len(notas_Aluno)
+
+    print('----------MÉDIA----------')
+    print(f"Media do Aluno Rm{rm}: {media_notas:.2f}")
+
+    print('----------SITUACAO----------')
+
+    if faltas > 18:
+        print(f'O aluno RM{rm} está Reprovado!')
+        break
+
+    if media_notas < 6:
+        print(f'Encaminhado para o Exame Final!')
+        exame = float(input("Digite sua nota no exame final!"))
+
+        if exame < 6:
+            print(f'O aluno RM{rm} está Reprovado!')
+        else:
+            print(f'O Aluno RM{rm} está Aprovado!!')
+        break
+
+    else:
+        print(f'O Aluno RM{rm} está Aprovado!!')
+        break
+
+input("ENTER para sair...")
+
+
+
+
+
+
+
+
     
-# calculo da média
-media_notas = sum(notas_Aluno.values()) / len(notas_Aluno)
-
-print(f"\nMedia do Aluno Rm{rm}: {media_notas:.2f}") # Adicionado :.2f para mostrar 2 casas decimais
 
 
-#Aprovado ou Reprovado
-if media_notas >= 6:
-    print(f"Aprovado!! Parabéns aluno {nome}!")
-else:
-    print("Reprovado...")
 
-print(f"\nUma notificação foi enviada ao email: {email}")
+
+
+
 
 input('\naperte ENTER para sair')   
